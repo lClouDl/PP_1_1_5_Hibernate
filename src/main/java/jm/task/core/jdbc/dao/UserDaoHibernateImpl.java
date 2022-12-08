33370a8c4d1925d249aed437bb6d84c8bf95a1f8
Konstantin Graf <cloud_33@mail.ru>
2022-12-08 23:37:41 +0700

    Изменения в классе UserDaoJDBCImpl: 1. Убрал блоки try из блоков try. 2. Добавил блоки finally. Изменения в классе UserDaoHibernateImpl: 1. В классе User с помощью аннотаций связал этот класс с таблицей бд 2. В классе Util добавлен метод getSessionFactory(), для создания фабрики сессий. 3. В классе UserServiceImpl поле userDao теперь создается на основе класса UserDaoHibernateImpl. 4. В классе UserDaoHibernateImpl реализованны все методы с помощью hibernate, согласно заданию.

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,47 +1,103 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private Util util;
+    public SessionFactory sessionFactory;
     public UserDaoHibernateImpl() {
-
+        this.util = new Util();
+        this.sessionFactory = util.getSessionFactory();
     }
 
 
     @Override
     public void createUsersTable() {
-
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(45), lastName VARCHAR(45), age TINYINT)").executeUpdate();
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            System.err.println("Ошибка при создании таблицы в базе данных.");
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            System.err.println("Ошибка при удалении таблицы из базы данных.");
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            System.err.println("Ошибка при добавлении User'а в базу данных");
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            session.delete(session.get(User.class, id));
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            System.err.println("Ошибка при удалении User'а по id.");
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> allUsers = new ArrayList<>();
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            allUsers = session.createQuery("from User").getResultList();
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            System.err.println("Ошибка при изъятии всех User'ов из таблицы в список.");
+        }
+        return allUsers;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            session.createQuery("delete User").executeUpdate();
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            System.err.println("Ошибка при очистке таблицы.");
+        }
     }
 
     @Override
     public void close() {
-
+        try {
+            sessionFactory.close();
+        } catch (HibernateException e) {
+            System.err.println("Ошибка при закрытии соединения с базой данных.");
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,10 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
@@ -20,6 +25,22 @@ public class Util {
             System.out.println("Во время соединения с базой данных произошла ошибка!");
         }
         return null;
+    }
+    public SessionFactory getSessionFactory() {
+        SessionFactory sessionFactory = null;
+        try {
+            sessionFactory = new Configuration().addAnnotatedClass(User.class)
+                    .setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver")
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .setProperty("hibernate.connection.url", URL)
+                    .setProperty("hibernate.connection.username", USERNAME)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .setProperty("hibernate.current_session_context_class", "thread")
+                    .setProperty("hibernate.show_sql", "true").buildSessionFactory();
+        } catch (HibernateException e) {
+            System.err.println("Во время соединения с базой данных произошла ошибка!");
+        }
+        return sessionFactory;
     }
 
 }
